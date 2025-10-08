ef9465b9ab909296e4b4c1af2108d48457f10bc8
An Nguyen <anngntien@gmail.com>
2025-10-08 23:43:08 +0700

    Restructure

---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <errno.h>
+using namespace std;
+
+int main() {
+    cout << "Hello!";
+}
